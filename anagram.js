2d4d5b2fe8b6dfbef85258bc30a67eaab2c95acf
sccala <sccala18@gmail.com>
2022-01-31 23:32:44 -0800

    anagram solution

--- anagram.js
+++ anagram.js
@@ -1,17 +1,33 @@
 // Given 2 strings, write a function to determine if the second string is an anagram of the first. An anagram is a word. phrase, or name formed by rearranging the letters of another, such as cinema, formed from iceman.
 
-function validAnagram(val1, val2) {
-  if (val1.length != val2.length) {
+function validAnagram(first, second) {
+  if (first.length !== second.length) {
     return false
-  } else {
-    let anagram1 = {}
-    let anagram2 = {}
   }
-}
+  const lookup = {}
 
-validAnagram('', '') //true
-validAnagram('aaz', 'zza') //false
-validAnagram('anagram', 'nagaram') //true
-validAnagram('rat', 'car') //false
-validAnagram('awesome', 'awesom') //true
-validAnagram('qwerty', 'qeywrt') //true
+  for (let i = 0; i < first.length; i++) {
+    let letter = first[i]
+    // if letter exists, increment. Otherwise set to 1
+    lookup[letter] ? (lookup[letter] += 1) : (lookup[letter] = 1)
+  }
+  // {a:3, n:1, g:1, r:1, m:1}
+  
+  for (let i = 0; i < second.length; i++) {
+    // can't find letter or letter is zero tehn it's not an anagram
+    let letter = second[i]
+    if (!lookup[letter]) {
+      return false
+    } else {
+      lookup[letter] -= 1
+    }
+  }
+  return true
+}
+console.log(validAnagram('aaz', 'zza')) //false
+console.log(validAnagram('', '')) //true
+console.log(validAnagram('aaz', 'zza')) //false
+console.log(validAnagram('anagram', 'nagaram')) //true
+console.log(validAnagram('rat', 'car')) //false
+console.log(validAnagram('awesome', 'awesom')) //false
+console.log(validAnagram('qwerty', 'qeywrt')) //true
