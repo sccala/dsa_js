b5d212227f469d4e62e6cdf2c99fa7309d32ee85
Chase <sccala18@gmail.com>
2022-04-03 23:04:16 -0700

    add new exercise

---
+++ Exercise/Exercise3.js
@@ -0,0 +1,18 @@
+// Write a function called sameFrequency. Given two positive integers, find out if the two numberts have the same frequency of digits. Your solution MUST have the following compleities:
+// Time: O(N)
+// Sample Input:
+// sameFrequency(182,281)//true
+// sameFrequency(34,14)//false
+// sameFrequency(3589578,5879385)//true
+// sameFrequency(22,222)//false
+
+function sameFrequency(int1, int2) {
+  if (int.length !== int2) {
+    false
+  }
+  for (i = 0; i < int1.length; i++) {
+    let j = i + 1
+    let array = {}
+    array.push(int1[i])
+  }
+}
